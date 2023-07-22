8ddb9bd919eb9a6953b02231aa8814c40565fb30
husseininjass <hussein.husam7@gmail.com>
2023-07-22 21:33:51 +0300

    fix the icons

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let home = document.querySelector("#home");
 home.onclick = _ => alert("you have to register first")
-window.oncontextmenu = (e) => e.preventDefault();
+// window.oncontextmenu = (e) => e.preventDefault();
 
 let dark = document.querySelector("#darkmodebtn")
 let box = document.querySelector("#box")
@@ -9,6 +9,9 @@ dark.onclick = function () {
     document.body.classList.toggle("darktheme");
     box.classList.toggle("boxdark")
     if(document.body.classList.contains("darkthem")){
-        photo.setAttribute("src" , "sun.png")
+        photo.setAttribute("src" , "sun.png");
+    }
+    else{
+        photo.setAttribute("src" , "moon.png")
     }
 }
