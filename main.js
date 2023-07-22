64e72d3ab4de6d95236b78f3966a1bf778fb29ae
husseininjass <hussein.husam7@gmail.com>
2023-07-22 18:59:17 +0300

    dark mode first update

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 let home = document.querySelector("#home");
 home.onclick = _ => alert("you have to register first")
-// window.oncontextmenu = (e) => e.preventDefault();
+window.oncontextmenu = (e) => e.preventDefault();
+
+let dark = document.querySelector("#darkmodebtn")
+dark.onclick = function () {
+    document.body.classList.toggle("darktheme")
+}
