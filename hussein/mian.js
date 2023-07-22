5c72c8c83bbf4eca1648a974cf8419f9f183e4d1
husseininjass <hussein.husam7@gmail.com>
2023-07-22 17:41:10 +0300

    home-page first update

---
+++ hussein/mian.js
@@ -0,0 +1,5 @@
+let a = document.getElementById("btn")
+let b = document.getElementById("move")
+a.onclick = function () {
+    b.click();
+}
--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 let home = document.querySelector("#home");
 home.onclick = _ => alert("you have to register first")
+// window.oncontextmenu = (e) => e.preventDefault();
