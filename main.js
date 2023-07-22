7f8ab6fca2a59817e2f66db6edb5d48218e0194d
husseininjass <hussein.husam7@gmail.com>
2023-07-22 21:07:44 +0300

    darkmode

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ home.onclick = _ => alert("you have to register first")
 window.oncontextmenu = (e) => e.preventDefault();
 
 let dark = document.querySelector("#darkmodebtn")
+let box = document.querySelector("#box")
+let photo = document.querySelector("#darkmode")
 dark.onclick = function () {
-    document.body.classList.toggle("darktheme")
+    document.body.classList.toggle("darktheme");
+    box.classList.toggle("boxdark")
+    if(document.body.classList.contains("darkthem")){
+        photo.setAttribute("src" , "sun.png")
+    }
 }
