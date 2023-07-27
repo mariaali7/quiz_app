9de869c9d499212a53f2f2eaa5296c1ae1a27afd
husseininjass <hussein.husam7@gmail.com>
2023-07-27 11:46:38 +0300

    first change

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 let home = document.querySelector("#home");
+
 home.onclick = _ => alert("you have to register first")
 // window.oncontextmenu = (e) => e.preventDefault();
 
 let dark = document.querySelector("#darkmodebtn")
-let box = document.querySelector("#box")
+let box = document.getElementById("box")
 let photo = document.querySelector("#darkmode")
 dark.onclick = function () {
     document.body.classList.toggle("darktheme");
     // box.classList.toggle("boxdark")
     if(document.body.classList.contains("darktheme")){
         photo.src = "sun.png"
+        box.style.backgroundImage = "url('bookdark.jpg')"
+        console.log(box);
     }
     else{
         photo.src = "moon.png"
+        box.style.backgroundImage = "url('booklight.jpg')"
     }
 }
 
